0d7298d79d7aa794f9506060fa491359dfbac2e7
sorosz89 <os.sandor@gmail.com>
2018-04-21 15:05:58 +0200

    Update hooks.js

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -2,21 +2,20 @@
 
 require('chromedriver');
 require('cucumber').Util.Colors(true);
-var chai = require('chai'), chaiAsPromised = require('chai-as-promised');
+
+const chai = require('chai'), chaiAsPromised = require('chai-as-promised');
+const webdriver = require('selenium-webdriver');
+
 chai.use(chaiAsPromised);
 global.expect = chai.expect;
-
-var webdriver = require('selenium-webdriver');
 global.by = webdriver.By;
 
-module.exports = function(){
+module.exports = () => {
     this.setDefaultTimeout(60000);
     this.registerHandler('BeforeFeatures', () => {
         global.driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
         return global.driver.manage().window().maximize();
-    });
-    
+    });    
     this.Before({tags: ['@phone']}, () => global.driver.manage().window().setSize(800, 640));
-
     this.registerHandler('AfterFeatures', () =>  global.driver.quit());
 };
